76ba61b7970d843150feb0882d67c0cc0b9bf325
gerardalbertcanada <gerard.a.canada@gmail.com>
2026-01-12 20:11:50 +0800

    Update config.js

--- api/config.js
+++ api/config.js
@@ -4,5 +4,5 @@
 module.exports = {
   CLIENT_ID: '491f6b8dbb80a91222bb24dc8f69144f64ebb489f4d032199846d7bc721e6c51',
   CLIENT_SECRET: '700c0c64a12e3b0b5df39af22fb3544f8d42a5d0488d613c88311df899a1d0c6',
-  SITE_URL: 'https://freshbooks-tracker-pipuaswg8-gerards-projects-1f3ffdb3.vercel.app'  // Update after first deploy
+  SITE_URL: 'https://freshbooks-tracker.vercel.app'  // Update after first deploy
 };
