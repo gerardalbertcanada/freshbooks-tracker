cd8033ed8706a1c6b164194518b7fa19d7fff1b5
gerardalbertcanada <gerard.a.canada@gmail.com>
2026-01-12 21:55:35 +0800

    Update auth.js

--- api/auth.js
+++ api/auth.js
@@ -5,7 +5,7 @@ module.exports = (req, res) => {
   authUrl.searchParams.set('response_type', 'code');
   authUrl.searchParams.set('client_id', CLIENT_ID);
   authUrl.searchParams.set('redirect_uri', `${SITE_URL}/api/callback`);
-  authUrl.searchParams.set('scope', 'user:profile:read user:projects:read user:time_entries:read user:bills:read user:billable_items:read user:business:read');
+  authUrl.searchParams.set('scope', 'user:profile:read user:projects:read user:time_entries:read user:bills:read user:billable_items:read user:business:read user:reports:read');
 
   res.redirect(302, authUrl.toString());
 };
